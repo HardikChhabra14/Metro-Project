702c20dc84833a701bb8110be767b4488a5ee321
HardikChhabra14 <hardik.chhabra1410@gmail.com>
2026-01-11 10:54:51 +0530

    Fix Django settings module and SITE_ID for production

--- metro/metro/wsgi.py
+++ metro/metro/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metro.metro.settings')
 
 application = get_wsgi_application()
