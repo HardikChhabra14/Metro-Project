343d3a317810a2faa3000d363975c5b288cda275
HardikChhabra14 <hardik.chhabra1410@gmail.com>
2026-01-15 08:48:14 +0000

    Fix docker paths and gunicorn module

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
 echo "Waiting for database..."
-
-# Wait for PostgreSQL to be ready
 while ! nc -z db 5432; do
-  sleep 0.5
+  sleep 1
 done
 
 echo "PostgreSQL started"
 
-# Run migrations and collect static files
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 exec "$@"
-export DJANGO_SETTINGS_MODULE=metro.settings
